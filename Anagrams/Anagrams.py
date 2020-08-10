94b6d0de1ca2eef7ee81646859d208876bd69740
Arpit Somani <arpit.somanias8@gmail.com>
2020-08-10 13:15:16 +0530

    Anagrams

---
+++ Anagrams/Anagrams.py
@@ -0,0 +1,25 @@
+"""
+@author: Arpit Somani
+"""
+
+str1=input("Enter the first string")
+str2=input("Enter the second string")
+
+count1=0
+i=0
+while(i<len(str1)):
+    count1=count1+ord(str1[i])
+    i=i+1    
+print(count1)
+
+count2=0
+i=0
+while(i<len(str2)):
+    count2=count2+ord(str2[i])
+    i=i+1
+print(count2)
+
+if (count1==count2):
+    print("These are anagrams")
+else:
+    print("These are not anagrams")
